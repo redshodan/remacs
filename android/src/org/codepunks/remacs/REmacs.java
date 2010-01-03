6998adac8b178e31e98129eba562d71dd053cba9
baron <baron@localhost.localdomain>
2010-01-03 15:22:09 -0700

    adding start of android client app.

---
+++ android/src/org/codepunks/remacs/REmacs.java
@@ -0,0 +1,15 @@
+package org.codepunks.remacs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class REmacs extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
